921c69abdbe75b30ab1c3e3f7a789dd98475e585
mdishamthaagam <mohamedisham@thaagam.email>
2025-08-13 20:50:31 +0530

    Initial commit for Isham project

---
+++ Controllers/Compose.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Invoicebook.Controllers
+{
+    public class ComposeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/script.js
@@ -0,0 +1,25 @@
+﻿function isNumber(evt) {
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
+function isDecimalNumber(evt) {
+    var inpValue = $(this).val();
+    if (!inpValue.match(/^[0-9]*([.,][0-9]+)?$/))
+        return false;
+    return true;
+}
+function isNoSpace(evt) {
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode == 32)
+        return false;
+    return true;
+}
+
+function isPhone(evt) {
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57) && charCode != 43)
+        return false;
+    return true;
+}
